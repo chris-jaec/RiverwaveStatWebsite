f96a1df54bab59839afdfe57a1eca0f6f8e2613e
Christian Jaeckel <christian.jaeckel@metafinanz.de>
2021-12-22 13:19:22 +0100

    no caching

--- api.py
+++ api.py
@@ -1,9 +1,7 @@
 import json
-import requests_cache
+import requests
 
 API_BASEURL = "https://xyz.execute-api.eu-central-1.amazonaws.com/dev"
-
-SESSION = requests_cache.CachedSession(expire_after=120, backend="memory")
 
 def api_wave_info(wave: str):
 
@@ -12,6 +10,6 @@ def api_wave_info(wave: str):
         'Content-Type': 'application/json'
     }
 
-    response = SESSION.get(url, headers=headers, timeout=30)
+    response = requests.get(url, headers=headers, timeout=30)
 
     return json.loads(response.content)
