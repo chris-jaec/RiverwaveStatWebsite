43ea4fcfee2680b266bfead09c16e3a7bcc9e0dc
Christian Jaeckel <christian.jaeckel@metafinanz.de>
2022-02-09 11:38:58 +0100

    snap!

--- api.py
+++ api.py
@@ -2,14 +2,24 @@ import json
 
 import requests
 
-API_ID = "a5acu7cfgd"
-API_STAGE = "prod"
+API_ID = "3qkwfo3i7e"
+API_STAGE = "dev"
 API_REGION = "eu-central-1"
 API_BASEURL = f"https://{API_ID}.execute-api.{API_REGION}.amazonaws.com/{API_STAGE}"
 
 
 def api_wave_info(wave: str):
     url = f"{API_BASEURL}/riverwaves/{wave}"
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.get(url, headers=headers, timeout=30)
+
+    return json.loads(response.content)
+
+def api_wave_overview():
+    url = f"{API_BASEURL}/riverwaves/"
     headers = {
         'Content-Type': 'application/json'
     }
