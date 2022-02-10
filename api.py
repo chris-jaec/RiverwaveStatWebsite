beb0281a4122b4b54ad3fa5127208305997ade36
chris-jaec <58209763+chris-jaec@users.noreply.github.com>
2022-02-10 10:17:49 +0100

    Refactor + Swiss Waves
    Refactoring + Wave Thun + Bremgarten added

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import json
 
 import requests
 
-API_ID = "a5acu7cfgd"
+API_ID = "xyz123"
 API_STAGE = "prod"
 API_REGION = "eu-central-1"
 API_BASEURL = f"https://{API_ID}.execute-api.{API_REGION}.amazonaws.com/{API_STAGE}"
@@ -10,6 +10,17 @@ API_BASEURL = f"https://{API_ID}.execute-api.{API_REGION}.amazonaws.com/{API_STA
 
 def api_wave_info(wave: str):
     url = f"{API_BASEURL}/riverwaves/{wave}"
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.get(url, headers=headers, timeout=30)
+
+    return json.loads(response.content)
+
+
+def api_wave_overview():
+    url = f"{API_BASEURL}/riverwaves/"
     headers = {
         'Content-Type': 'application/json'
     }
